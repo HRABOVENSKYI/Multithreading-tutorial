1c01cc9f8a7e509675a23ba9b57144068d55cb15
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-07-01 13:58:46 +0300

    Example of logging from 4 threads

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,13 +12,15 @@ class Printer implements Runnable { // We can also extend class Thread, but this
 public class App {
     public static void main(String[] args) {
 
-        // Printing happens simultaneously from Thread-0 and main threads. Outputs are mixed.
+        // Printing happens simultaneously all threads. Outputs are mixed.
 
-        Thread thread = new Thread(new Printer());
-        thread.start(); // logging to console
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println(Thread.currentThread().getName() + " " + i); // logging to console
-        }
+        Thread thread1 = new Thread(new Printer()); // Thread-0
+        Thread thread2 = new Thread(new Printer()); // Thread-1
+        Thread thread3 = new Thread(new Printer()); // Thread-2
+        Thread thread4 = new Thread(new Printer()); // Thread-3
+        thread1.start(); // logging to console
+        thread2.start(); // logging to console
+        thread3.start(); // logging to console
+        thread4.start(); // logging to console
     }
 }
